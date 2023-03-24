3229046505b38e89b07ccec5807a8e58c5bc7e82
Prachi <prachikale136@gmail.com>
2023-03-24 17:57:34 +0530

    [Prachi] Add:

---
+++ DataStructureAlgorithm/DataStructureAlgorithm/InsertionSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    internal class InsertionSort
+    {
+        public static void Sort()
+        {
+            //integer array
+            int[] array = new int[5] { 10, 2, 1, 6, 4 };
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            
+            for(int i = 1; i < array.Length; i++) 
+            {
+                
+                int value = array[i];
+                int j = i - 1;
+                //check whether left elemnet is greater or less than current elemnt
+                while((j>=0) && (array[j].CompareTo(value) > 0))
+                {
+                    //moving left side elemnt to the forward direction
+                    array[j+1] = array[j];
+                    j--;
+                }
+                //stored correct elemnts to its correct position
+                 array[j + 1] = value;
+            }
+            Console.WriteLine("after soeting element is:");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        
+    }
+}
